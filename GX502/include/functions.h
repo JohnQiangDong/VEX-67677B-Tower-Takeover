9e98e72c17d544f56f9d5890a4e991d39329c72b
JohnQiangDong <32593035+JohnQiangDong@users.noreply.github.com>
2019-11-12 19:08:52 +0800

    more motion funtions

--- GX502/include/functions.h
+++ GX502/include/functions.h
@@ -11,7 +11,7 @@ void arm_move(int velocity) {
 }
 // positive to push cubes
 void push_move(int velocity) {
-  push.spin(vex::directionType::fwd,velocity,velocityUnits::pct);
+  push.spin(vex::directionType::fwd, velocity, velocityUnits::pct);
 }
 // true for hold
 // false for coast
@@ -24,6 +24,30 @@ void hand_stop(bool stoptype) {
     hand2.stop(brakeType::hold);
   }
 }
-void arm_stop(bool stoptype){
-  
+void arm_stop(bool stoptype) {
+  if (stoptype == false) {
+    arm.stop(brakeType::coast);
+  } else {
+    arm.stop(brakeType::hold);
+  }
+}
+void push_stop(bool stoptype) {
+  if (stoptype == false) {
+    push.stop(brakeType::coast);
+  } else {
+    push.stop(brakeType::hold);
+  }
+}
+void robot_stop(bool stoptype) {
+  if (stoptype == false) {
+    left_1.stop(vex::brakeType::coast);
+    left_2.stop(vex::brakeType::coast);
+    right_1.stop(vex::brakeType::coast);
+    right_2.stop(vex::brakeType::coast);
+  } else {
+    left_1.stop(vex::brakeType::hold);
+    left_2.stop(vex::brakeType::hold);
+    right_1.stop(vex::brakeType::hold);
+    right_2.stop(vex::brakeType::hold);
+  }
 }
