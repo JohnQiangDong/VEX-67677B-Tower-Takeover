ea0b8863727271b29eae898eb048f9d49b1cd3b5
krahets <407891080@qq.com>
2019-11-21 14:02:51 +0800

    Add PID test.

--- GX502/include/auto_bs.h
+++ GX502/include/auto_bs.h
@@ -5,7 +5,8 @@
 #include "automove.h"
 #include "utils.h"
 
-void auto_bs(){
+void auto_bs()
+{
   /*
   push.spin(directionType ::fwd,50,velocityUnits::pct);
   push.setMaxTorque(1.0, currentUnits:: amp);
@@ -31,70 +32,72 @@ void auto_bs(){
   arm.setMaxTorque(0.2, currentUnits::amp);hand1.spin(directionType::fwd,100,vex::velocityUnits::pct);
   */
   vexDelay(2000);
-  arm.spin(directionType::rev,5,vex::velocityUnits::pct);
-  arm.setMaxTorque(0.4,currentUnits::amp);
+  arm.spin(directionType::rev, 5, vex::velocityUnits::pct);
+  arm.setMaxTorque(0.4, currentUnits::amp);
   push.stop(brakeType::hold);
-  push.setMaxTorque(0.2,currentUnits::amp);
-  hand1.spin(directionType::fwd,100,vex::velocityUnits::pct);
-  hand2.spin(directionType::rev,100,vex::velocityUnits::pct);
+  push.setMaxTorque(0.2, currentUnits::amp);
+  hand1.spin(directionType::fwd, 100, vex::velocityUnits::pct);
+  hand2.spin(directionType::rev, 100, vex::velocityUnits::pct);
   //collect frist 3 cubes
-  automove(1350,1350,6000,6000);
-
+  automove(1350, 1350, 6000, 6000);
 
   hand1.stop(vex::brakeType::hold);
   hand2.stop(vex::brakeType::hold);
 
-  hand1.setMaxTorque(0.1,currentUnits::amp);
-  hand2.setMaxTorque(0.1,currentUnits::amp);
+  hand1.setMaxTorque(0.1, currentUnits::amp);
+  hand2.setMaxTorque(0.1, currentUnits::amp);
   //stop hand
-  automove(119,-119,2000,14000);
-  autoturn(-1390,-1390,3000,9900);
+  automove(119, -119, 2000, 14000);
+  autoturn(-1390, -1390, 3000, 9900);
   //go back
   automove(-144, 144, 2000, 14000);
   //automove(-200,-200,2000,10000);
-  
+
   //start to collect another four cubes
-  hand1.setMaxTorque(2.4,currentUnits::amp);
-  hand2.setMaxTorque(2.4,currentUnits::amp);
-  hand1.spin(directionType::fwd,100,vex::velocityUnits::pct);
-  hand2.spin(directionType::rev,100,vex::velocityUnits::pct);
-  automove(1200,1200,7000,3500);  
+  hand1.setMaxTorque(2.4, currentUnits::amp);
+  hand2.setMaxTorque(2.4, currentUnits::amp);
+  hand1.spin(directionType::fwd, 100, vex::velocityUnits::pct);
+  hand2.spin(directionType::rev, 100, vex::velocityUnits::pct);
+  automove(1200, 1200, 7000, 3500);
   vexDelay(70);
   //dalay for get cube at right position
   hand1.stop(vex::brakeType::hold);
   hand2.stop(vex::brakeType::hold);
-  hand1.setMaxTorque(0.1,currentUnits::amp);
-  hand2.setMaxTorque(0.1,currentUnits::amp);
+  hand1.setMaxTorque(0.1, currentUnits::amp);
+  hand2.setMaxTorque(0.1, currentUnits::amp);
   automove(-705, -705, 2000, 14000);
   automove(-420, 420, 1000, 9900);
   automove(400, 400, 4000, 6000);
-  
-  push.setMaxTorque(2.4, currentUnits::amp);  
-  push.spin(vex::directionType::fwd,43,vex::velocityUnits::pct);
+
+  push.setMaxTorque(2.4, currentUnits::amp);
+  push.spin(vex::directionType::fwd, 43, vex::velocityUnits::pct);
   vexDelay(1740);
   push.stop(brakeType::coast);
-  hand1.spin(directionType::rev,100,vex::velocityUnits::pct);
-  hand2.spin(directionType::fwd,100,vex::velocityUnits::pct);
-  vexDelay(100);//prevent hand stucking cubes
-  
+  hand1.spin(directionType::rev, 100, vex::velocityUnits::pct);
+  hand2.spin(directionType::fwd, 100, vex::velocityUnits::pct);
+  vexDelay(100); //prevent hand stucking cubes
+
   automove(-200, -200, 1000, 14000);
   hand1.stop(vex::brakeType::hold);
   hand2.stop(vex::brakeType::hold);
-  hand1.setMaxTorque(0.1,currentUnits::amp);
-  hand2.setMaxTorque(0.1,currentUnits::amp);  
+  hand1.setMaxTorque(0.1, currentUnits::amp);
+  hand2.setMaxTorque(0.1, currentUnits::amp);
 }
 
-void test(){
-Spin(push,vex::directionType::rev,5,2.0);
-Spin(arm,vex::directionType::rev,5,2.0);
-Spin(hand1,vex::directionType::fwd,80,2.2);
-Spin(hand2,vex::directionType::fwd,80,2.2);
-vexDelay(500);
-Stop(hand1, brakeType::coast,0.1);
-Stop(hand2,brakeType::coast,0.1);
-Stop(arm,brakeType::hold,0.2);
-Stop(push, brakeType::hold,0.2);
-}
+void test()
+{
+  // Spin(push, vex::directionType::rev, 5, 2.0);
+  // Spin(arm, vex::directionType::rev, 5, 2.0);
+  // Spin(hand1, vex::directionType::fwd, 80, 2.2);
+  // Spin(hand2, vex::directionType::fwd, 80, 2.2);
+  // vexDelay(500);
+  // Stop(hand1, brakeType::coast, 0.1);
+  // Stop(hand2, brakeType::coast, 0.1);
+  // Stop(arm, brakeType::hold, 0.2);
+  // Stop(push, brakeType::hold, 0.2);
 
+  moveTarget(500, 80, 0.1, 0.01, 0.3); // tar, max_pct, kp, kd, ki
+  turnTarget(500, 60, 0.1, 0.01, 0.3); // tar, max_pct, kp, kd, ki
+}
 
 #endif
